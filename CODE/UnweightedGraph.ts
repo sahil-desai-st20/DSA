bb2fbf5d92a6e828e4b3ec90b18a1d3abe71fc00
sahil-desai-st20 <your.email@example.com>
2026-02-27 13:46:23 +0530

    Implement UnweightedGraph class with methods to add vertices, edges, and perform BFS traversal

---
+++ CODE/UnweightedGraph.ts
@@ -0,0 +1,40 @@
+class UnweightedGraph<T>{
+    private vertices:Map<T,Set<T>>;
+    private isDirect:boolean;
+   
+     constructor(isDirect:boolean=false){
+        this.vertices=new Map()
+        this.isDirect=isDirect
+     }
+    addVertex(vertex:T):void{
+        if(!this.vertices.has(vertex)){
+            this.vertices.set(vertex, new Set())
+        }else console.log("vertex already exists")
+     }
+     addEdge(source:T,destination:T):void{
+      this.addVertex(source)
+      this.addVertex(destination)
+      this.vertices.get(source)?.add(destination);
+      if(!this.isDirect){
+         this.vertices.get(destination)?.add(source)
+      }
+   }
+      bfs(start:T):T[]{
+      if(!this.vertices.has(start)) return []  
+      const visited =new Set<T>();
+      const result:T[]=[];
+      const queue:T[]=[start]
+      while (queue.length>0){
+         const vertex=queue.shift()
+        if (vertex === undefined) continue;  // Skip undefined values
+         result.push(vertex)
+         for (const neigh of this.vertices.get(vertex)!){
+            if(!visited.has(neigh)){
+               visited.add(neigh)
+               queue.push(neigh)    
+            }
+         }
+      }
+      return result;
+     }
+}
